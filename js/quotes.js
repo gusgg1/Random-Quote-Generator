373e683cc45fa7dacc0725ba012e1ae15c1d9820
Gusgus1 <g.a.sidharta1@gmail.com>
2017-11-27 15:56:48 +0100

    finished project

---
+++ js/quotes.js
@@ -0,0 +1,21 @@
+
+// Objects in array include additional properties, such as categorization tags.
+var quotes = [
+  {quote: 'Success is stumbling from failure to failure with no loss of enthusiasm.', source: 'Winston S. Churchill', citation: 'The Second World War', year: 1948, tags: 'bravery'},
+  {quote: 'Live as if you were to die tomorrow. Learn as if you were to live forever.', source: 'Mahatma Gandhi', citation: 'The Essential Gandhi: An Anthology of His Writings on His Life, Work, and Ideas', year: 1962, tags: 'learning'},
+  {quote: 'Do unto others as you would have others do unto you', source: 'Jesus Christ', citation: 'The Bible', year: '33 A.D', tags: 'peace'},
+  {quote: 'For myself I am an optimist - it does not seem to be much use to be anything else.', source: 'Winston S. Churchill', citation: 'The Second World War', year: 1948, tags: 'Inspirational'},
+  {quote: 'An eye for an eye will only make the whole world blind.', source: 'Mahatma Gandhi', citation: 'The Story of My Experiments With Truth', year: 1940, tags: 'peace'},
+  {quote: 'For what shall it profit a man, if he shall gain the whole world, and lose his own soul?', source: 'Jesus Christ', citation: 'The Bible - Mark 8:36', year: '33 A.D', tags: 'wisdom'},
+  {quote: 'Kites rise highest against the wind, not with it.', source: 'Winston S. Churchill', citation: 'My Early Life, 1874-1904', year: 1930, tags: 'succes'}
+];
+
+
+
+
+
+
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,93 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+
+// See quotes.js to find the data (array of objects called quotes).
+
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+// Extra credit for exceeds rating: 
+// calls the printQuote function every 30 seconds.
+setInterval(printQuote, 30000);
+
+// Returns a random quote object from the array 'quotes' in quotes.js
+function getRandomQuote() {
+  var quote = Math.floor(Math.random() * quotes.length);
+  return quotes[quote];
+}
+
+// Extra credit for exceeds rating: 
+// Returns a random number between 0 and including 255.
+function randonRGBNumber() {
+  return Math.floor(Math.random() * 256);
+}
+
+// Extra credit for exceeds rating: 
+// Builds the rgb-color, changes the color of the body and button elements.
+function changeColorPage() {
+  // Selecting body and button elements
+  var page = document.querySelector('body');
+  var button = document.getElementById('loadQuote');
+
+  // building rgb-color using the randomRGBNumber function
+  var rgbColor = 'rgb(' + randonRGBNumber();
+  rgbColor += ', ' + randonRGBNumber();
+  rgbColor += ', ' + randonRGBNumber() + ')';
+
+  // changing background color of body and button accordingly
+  page.style.backgroundColor = rgbColor;
+  button.style.backgroundColor = rgbColor;
+}
+
+// Prints a random quote when clicked on button or when called by setInterval, and also calls the changeColorPage function to change the background color of the page.
+function printQuote() {
+  // changing color of page and button
+  changeColorPage();
+
+  // getting random quote
+  var quote = getRandomQuote();
+
+  // selecting div element with the id quote-box
+  var quoteBox = document.getElementById('quote-box');
+
+  // building html string
+  var html = '<p class="quote">' + quote.quote + '</p>';
+  html += '<p class="source">' + quote.source;
+  html += '<span class="citation">' + quote.citation + '</span>';
+  html += '<span class="year">' + quote.year + '</span>';
+  html += '<span class="tags">' + quote.tags + '</span>';
+  html += '</p>';   
+
+  // displays HTML string to the page
+  quoteBox.innerHTML = html;
+}
+
+// Prints random quote to page and changes background color of page.
+printQuote();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
